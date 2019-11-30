3b1e3bb4631c42b0c6216f54a897eeb952c97ee5
Mateusz Belka <mateusz.belka14@gmail.com>
2019-11-30 04:43:50 +0100

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalcComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
