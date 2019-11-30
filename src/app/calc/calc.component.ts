df46ed9ac6daffc2172304a2e41ec85a7eb69dc5
Mateusz Belka <mateusz.belka14@gmail.com>
2019-11-30 07:13:27 +0100

    Functionality added, buttons still dont work

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -1,32 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { parse } from "querystring";
 
 @Component({
-  selector: 'app-calc',
-  templateUrl: './calc.component.html',
-  styleUrls: ['./calc.component.css']
+  selector: "app-calc",
+  templateUrl: "./calc.component.html",
+  styleUrls: ["./calc.component.css"]
 })
 export class CalcComponent implements OnInit {
-  numberOneBin = '';
-  numberTwoBin = '';
-  numberOneDecimal = '';
-  numberTwoDecimal = '';
-  sum = '';
-  constructor() { }
+  numberOneBin = "";
+  numberTwoBin = "";
+  numberOneDec = "";
+  numberTwoDec = "";
+  sumBin = "";
+  sumDec = "";
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  calcSum(){
-    let number1 = parseInt(this.numberOne,2);
-    let number2 = parseInt(this.numberTwo, 2);
-    let sum1 = number1 + number2;
-    return this.sum = sum1.toString(2);
-  }
-
-  checkNumber(event) {
-    if(event.key !== '1' && event.key !== 'Backspace' && event.key !== '0') {
-      event.preventDefault();
+  calcSumBin() {
+    if (this.numberOneBin !== "" && this.numberTwoBin !== "") {
+      let sum = parseInt(this.sumDec, 10);
+      this.sumBin = sum.toString(2);
     }
   }
 
+  calcSumDec() {
+    if (this.numberOneDec !== "" && this.numberTwoDec !== "") {
+      let sum = parseInt(this.numberOneDec) + parseInt(this.numberTwoDec);
+      this.sumDec = sum.toString();
+    }
+  }
+  convertToBinary() {
+    if(this.numberOneDec!== ""){
+      let number1 = parseInt(this.numberOneDec, 10);
+      this.numberOneBin = number1.toString(2);
+    }
+    if(this.numberTwoDec!== ""){
+      let number2 = parseInt(this.numberTwoDec, 10);
+      this.numberTwoBin = number2.toString(2);
+    }
+  }
+  convertToDecimal() {
+    if(this.numberOneBin!== ""){
+      let number1 = parseInt(this.numberOneBin, 2);
+      this.numberOneDec = number1.toString(10);
+    }
+    if(this.numberTwoBin!== ""){
+      let number2 = parseInt(this.numberTwoBin, 2);
+      this.numberTwoDec = number2.toString(10);
+    }
+  }
+  
+  makeCalcDec(){
+    this.convertToBinary();
+    this.calcSumDec();
+    this.calcSumBin();
+  }
+  makeCalcBin(){
+    this.convertToDecimal();
+    this.calcSumDec();
+    this.calcSumBin();
+  }
+  
+  checkNumberBin(event) {
+    if (event.key !== "1" && event.key !== "Backspace" && event.key !== "0") {
+      event.preventDefault();
+    }
+  }
+  checkNumberDecimal(event) {
+    if (event.key !== "1" && event.key !== "Backspace" && event.key !== "0" && event.key !== "2" && event.key !== "3" && event.key !== "4" && event.key !== "5" && event.key !== "6" && event.key !== "7" && event.key !== "8" && event.key !== "9") {
+      event.preventDefault();
+    }
+  }
 }
